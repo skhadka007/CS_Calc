55b38f65e8dfe0a7f3f0830aab7dfc3e8bbd3099
Santosh Khadka <skhadka007@hotmail.com>
2020-05-19 16:45:48 -0500

    Added Menu bar basics template

--- cs_calc.py
+++ cs_calc.py
@@ -1,4 +1,22 @@
-# Santosh Khadka
-# CS Calc - main file
-
+## Santosh Khadka
+## CS Calc - main file
 from tkinter import *
+
+## Root needs to be created FIRST
+root = Tk()
+
+## create the object/widget first then set it into the window
+label_File = Label(root, text="File")
+label_Edit = Label(root, text="Edit")
+label_View = Label(root, text="View")
+label_Tab = Label(root, text="Tab")
+
+label_File.grid(row=0, column=0)
+label_Edit.grid(row=0, column=1)
+label_Tab.grid(row=0, column=2)
+label_View.grid(row=0, column=3)
+
+
+## GUI program is constantly looping to check for changes - so create loop
+root.mainloop()
+
