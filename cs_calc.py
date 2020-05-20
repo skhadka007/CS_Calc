b0c0ca1dd6a491cbf1d181ba901f46f4fcddbdb1
Santosh Khadka <skhadka007@hotmail.com>
2020-05-20 15:05:27 -0500

    Syntax structure changes. Added entry box.

--- cs_calc.py
+++ cs_calc.py
@@ -5,19 +5,15 @@ from tkinter import *
 ## Root needs to be created FIRST
 root = Tk()
 
+## Entry Box
+inputBox = Entry(root, text="")
+
 ## Menu Bar
 label_File = Label(root, text="File")
 label_Edit = Label(root, text="Edit")
 label_View = Label(root, text="View")
 label_Tab = Label(root, text="Tab")
 label_change1 = Label(root, text="CHANGE")
-
-# Menu bar grid  
-label_File.grid(row=0, column=0)
-label_Edit.grid(row=0, column=1)
-label_Tab.grid(row=0, column=2)
-label_View.grid(row=0, column=3)
-label_change1.grid(row=0, column=4)
 
 ## Buttons: Digits 0-9, +x-%=, clear, x^2, x^3, 10^x, log, ln, n!, (), +/-, .,  
 button_0 = Button(root, text="0")
@@ -58,27 +54,37 @@ button_pi = Button(root, text="pi")
 button_div = Button(root, text="div")
 button_mod = Button(root, text="mod")
 
+# Entry box grid
+inputBox.grid(row=1, column=0, columnspan=5)
+
+# Menu bar grid  
+label_File.grid(row=0, column=0)
+label_Edit.grid(row=0, column=1)
+label_Tab.grid(row=0, column=2)
+label_View.grid(row=0, column=3)
+label_change1.grid(row=0, column=4)
+
 # Buttons grid
+button_7.grid(row=2, column=1)
+button_8.grid(row=2, column=2)
+button_9.grid(row=2, column=3)
 
-button_7.grid(row=1, column=1)
-button_8.grid(row=1, column=2)
-button_9.grid(row=1, column=3)
+button_4.grid(row=3, column=1)
+button_5.grid(row=3, column=2)
+button_6.grid(row=3, column=3)
 
-button_4.grid(row=2, column=1)
-button_5.grid(row=2, column=2)
-button_6.grid(row=2, column=3)
+button_1.grid(row=4, column=1)
+button_2.grid(row=4, column=2)
+button_3.grid(row=4, column=3)
+button_0.grid(row=5, column=1)
 
-button_1.grid(row=3, column=1)
-button_2.grid(row=3, column=3)
-button_3.grid(row=3, column=3)
-button_0.grid(row=4, column=1)
+button_divi.grid(row=2, column=4)
+button_mult.grid(row=3, column=4)
+button_subt.grid(row=4, column=4)
 
-button_divi.grid(row=1, column=4)
-button_mult.grid(row=2, column=4)
-
-button_POSorNEG.grid(row=4, column=2)
-button_equal.grid(row=4, column=3)
-button_addi.grid(row=4, column=4)
+button_POSorNEG.grid(row=5, column=2)
+button_equal.grid(row=5, column=3)
+button_addi.grid(row=5, column=4)
 
 
 #button_.grid(row=, column=)
@@ -87,6 +93,6 @@ button_addi.grid(row=4, column=4)
 
 
 
-## GUI program is constantly looping to check for changes - so create loop
+## GUI program is constantly looping to check for changes - Loop created
 root.mainloop()
 
